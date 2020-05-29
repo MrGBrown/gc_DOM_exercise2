43eec46fdaa418de87c2dbc4244479f4568ff3ae
garrett <garrett.brown2@loop.colum.edu>
2020-05-28 22:00:40 -0400

    Updated with Event Listeners

--- js/script.js
+++ js/script.js
@@ -1,27 +1,36 @@
-const boxMenu = document.querySelector(".section1");
+const boxMenu = document.querySelector('.section1');
+const hamburgerMenu = document.getElementById('hamburger');
 const aTagNumbers = document.getElementById('numberlink');
 const aTagLetters = document.getElementById('letterlink');
 
 aTagNumbers.addEventListener('click', function(e){
+    document.getElementById('letters').style.display = "none";
+    document.getElementById('numbers').style.display = "block";
+    boxMenu.style.display = "none";
     e.preventDefault();
 });
 
 aTagLetters.addEventListener('click', function(f){
-    f.preventDefault();
-});
-
-document.getElementById('hamburger').onclick = function showMenu() {   
-    document.querySelector(".section1").style.display = "block";
- };
-
-document.getElementById('numberlink').onclick = function showNumbers() {   
-    document.getElementById('letters').style.display = "none";
-    document.getElementById('numbers').style.display = "block";
-    boxMenu.style.display = "none";
- };
-
-document.getElementById('letterlink').onclick = function showLetters() {   
     document.getElementById('letters').style.display = "block";
     document.getElementById('numbers').style.display = "none";
     boxMenu.style.display = "none";
- };
+    f.preventDefault();
+});
+
+hamburgerMenu.addEventListener('click', function(g){
+    document.querySelector(".section1").style.display = "block";
+    g.preventDefault();
+});
+
+// document.getElementById('hamburger').onclick = function showMenu() {   
+//     document.querySelector(".section1").style.display = "block";
+// };
+
+// document.getElementById('numberlink').onclick = function showNumbers() {   
+
+// };
+
+// document.getElementById('letterlink').onclick = function showLetters() {   
+
+// };
+
